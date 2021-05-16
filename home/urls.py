af9904eeadc835697604b114677dcc2c44cc2a8e
Ravi Indorey <ravi.indorey90@gmail.com>
2021-05-17 04:06:20 +0530

    Ajax: user can like and unlike a post-link

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from .views import home, like_link_post
 app_name = 'home'
 
 urlpatterns = [
-    path('<int:link_post_id>/like', like_link_post, name='like_link_post'),
+    path('like/', like_link_post, name='like_link_post'),
     path('', home, name='homepage'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
+from common.decorators import ajax_required
 
 from .models import LinkPost
 from .forms import LinkPostForm
@@ -16,17 +19,38 @@ def home(request):
             link_post_obj.save()
 
     link_post_form = LinkPostForm()
+    user_likes = None
+    if request.user.is_authenticated:
+        user_likes = LinkPost.objects.filter(liked_by=request.user)
     return render(request, 'home/homepage.html', {
         'links': links,
         'link_post_form': link_post_form,
+        'user_likes': user_likes,
     })
 
 
-@login_required
+@ajax_required
 @require_POST
-def like_link_post(request, link_post_id):
-    link_post = get_object_or_404(LinkPost, id=link_post_id)
-    link_post.total_likes += 1
-    link_post.liked_by.add(request.user)
-    link_post.save()
-    return redirect('home:homepage')
+@login_required
+def like_link_post(request):
+    link_post_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if link_post_id and action:
+        try:
+            link_post = LinkPost.objects.get(id=link_post_id)
+            if action == 'like':
+                if not LinkPost.objects.filter(liked_by=request.user):
+                    link_post.total_likes += 1
+                    link_post.liked_by.add(request.user)
+            else:
+                if LinkPost.objects.filter(liked_by=request.user):
+                    link_post.total_likes -= 1
+                    link_post.liked_by.remove(request.user)
+            link_post.save()
+            return JsonResponse({
+                'status': 'ok',
+                'link_post_id': link_post_id
+            })
+        except LinkPost.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
