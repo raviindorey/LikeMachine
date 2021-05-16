488ae0ef1944bf5843c6d7f82aafac1ac092acef
Ravi Indorey <ravi.indorey90@gmail.com>
2021-05-16 22:28:26 +0530

    Add linkpost model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class LinkPost(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name='links',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=300)
+    link = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.title}'
