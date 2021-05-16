bed69bcf43b0d010b96f0b18f2df631d5099f2bd
Ravi Indorey <ravi.indorey90@gmail.com>
2021-05-16 22:33:03 +0530

    Register linkpost to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LinkPost
+
+
+@admin.register(LinkPost)
+class LinkPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'link')
+    search_fields = ('title', 'link')
